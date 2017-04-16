6e78f14f897f46a9c61d1d8dc124e926e41a2ee5
DLHKwan <danielleungdaniel@gmail.com>
2017-04-16 15:31:51 +0800

    no message

---
+++ Homework/104360098_week2hw2v2.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+void initial(int relations[][20]) {
+    int i, j;
+    for (i = 0; i < 20; i++) {
+        for (j = 0; j < 20; j++) {
+            if (j == 0) {
+                relations[i][j] = i;
+            }else {
+                relations[i][j] = -1;
+            }
+        }
+    }
+}
+void fillmap(int relations[][20], int a, int b, int n) {
+    int i, j, temp;
+    for (i = 0; i < n; i++) {
+        if (relations[b][i] == -1) {
+            relations[b][i] = a;
+            temp = i + 1;
+            break;
+        }
+    }
+    for (i = 0; i < n; i++) {
+        for (j = 1; j < n; j++) {
+            if (relations[i][j] == b) {
+                relations[i][j + 1] = a;
+            }
+        }
+    }
+    i = 1;
+    while (relations[a][i] != -1) {
+        relations[b][temp] = relations[a][i];
+        i++;
+        temp++;
+    }
+}
+void input_fill(int relations[][20], int n) {
+    int i, a, b;
+    for (i = 1; i <= n - 1; i++) {
+        scanf("%d %d", &a, &b);
+        fillmap(relations, a, b, n);
+    }
+}
+int getDistance(int relations[][20], int n, int i, int j) {
+    int iindex, jindex;
+    for (iindex = 0; relations[i][iindex] != -1; iindex++) {
+        for (jindex = 0; relations[j][jindex] != -1; jindex++) {
+            if (relations[i][iindex] == relations[j][jindex]) {
+                return iindex + jindex;
+            }
+        }
+    }
+}
+void printresult(int relations[][20], int n) {
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            printf("%d-%d-%d\n", i, j, getDistance(relations, n, i, j));
+        }
+    }
+}
+void process() {
+    int relations[20][20], n;
+    initial(relations);
+    scanf("%d", &n);
+    input_fill(relations, n);
+    printrelations(relations, n);
+    printresult(relations, n);
+}
+int main(int argc, char **argv) {
+    process();
+    return 0;
+}
---
+++ Homework/104360098_week6hw1v2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+void printmap(int n, int map[][20]) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (map[i][j] == 2) {
+                printf("# ");
+            }else {
+                printf("%d ", map[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+void inputData(int *n, int map[][20]) {
+    int i, j;
+    scanf("%d", n);
+    for (i = 0; i < *n; i++) {
+        for (j = 0; j < *n; j++) {
+            scanf("%d", &map[i][j]);
+        }
+    }
+}
+void maze(int n, int map[][20], int x, int y) {
+    map[x][y] = 2;
+    if (x == n - 2 && y == n - 2) {
+        printmap(n, map);
+    }else {
+        if (map[x - 1][y] == 0) {
+            maze(n, map, x - 1, y);
+        }
+        if (map[x][y + 1] == 0) {
+            maze(n, map, x, y + 1);
+        }
+        if (map[x + 1][y] == 0) {
+            maze(n, map, x + 1, y);
+        }
+        if (map[x][y - 1] == 0) {
+            maze(n, map, x, y - 1);
+        }
+    }
+    map[x][y] = 0;
+}
+void process() {
+    int n, map[20][20];
+    inputData(&n, map);
+    maze(n, map, 1, 1);
+}
+int main(int argc, char **argv) {
+    process();
+    return 0;
+}
